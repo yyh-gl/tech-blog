a7d9eca89bebfc0d69b11d47b7235a4505ddcf81
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-22 23:59:59 +0900

    自己紹介記事 公開

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Start deploy..."
+
+# Build the project.
+hugo
+
+# Go to public folder
+cd public
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come back
+cd ..
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+echo "Done!"
+
