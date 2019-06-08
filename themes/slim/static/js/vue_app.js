4a49d2e24b5fbd9f98b4403539f501019aa1b80d
yyh-gl <yhonda.95.gl@gmail.com>
2019-06-08 17:40:14 +0900

    いいね機能追加

--- themes/slim/static/js/vue_app.js
+++ themes/slim/static/js/vue_app.js
@@ -1,12 +1,32 @@
 Vue.component('good-counter', {
-  template: '<button onclick="">\n' +
+  template: '<button v-on:click="addCount">\n' +
     '<i class="far fa-thumbs-up"></i> いいね　{{ good_count }}\n' +
     '</button>',
   data: function () {
     return {
-      good_count: 9999
+      good_count: "-",
     }
   },
+  mounted () {
+    let paths = location.pathname.split('/');
+    let reqUrl = 'https://super.hobigon.work/api/v1/blogs/' + paths[paths.length - 2];
+
+    axios
+      .get(reqUrl)
+      .then(response => this.good_count = response.data.post.count)
+  },
+  methods: {
+    addCount: function (event) {
+      let paths = location.pathname.split('/');
+      let reqUrl = 'https://super.hobigon.work/api/v1/blogs/' + paths[paths.length - 2] + '/good';
+
+      if(event) {
+        axios
+          .post(reqUrl)
+          .then(response => this.good_count = response.data.after)
+      }
+    }
+  }
 });
 
 // root インスタンスを作成する
