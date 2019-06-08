0aed097b47a7880258e16046659f41134c22d7dc
yyh-gl <yhonda.95.gl@gmail.com>
2019-06-08 22:25:17 +0900

    【修正】記事コード：good_api

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ cd public &&
 
 # ブログ設定用リポジトリへのPUSH
 echo -e "\nブログ設定用リポジトリへPUSH" &&
-msg="【公開】記事コード：$1"
+msg="【修正】記事コード：$1"
 git add -A &&
 git commit -m "$msg" &&
 git push origin master &&
@@ -30,11 +30,6 @@ git add -A &&
 git commit -m "$msg" &&
 git push origin master &&
 echo "GitHub Pages用リポジトリへPUSH完了" &&
-
-# 新規記事をサーバに登録
-echo -e "\nサーバに記事を登録" &&
-curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"$1\"}" https://super.hobigon.work/api/v1/blogs &&
-echo -e "\nサーバに記事を登録完了" &&
 
 echo -e "\nデプロイ完了！"
 
