58ca94cacc0b77e2199886db48929b4ad2a6e4f6
yyh-gl <yhonda.95.gl@gmail.com>
2019-06-09 22:15:00 +0900

    予約公開用スクリプト追加

---
+++ reservation_post.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/tech-blog-settings &&
+hugo &&
+cd public &&
+msg="【予約公開】記事コード：$1"
+git add -A &&
+git commit -m "$msg" &&
+git push origin master &&
+curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"$1\"}" https://super.hobigon.work/api/v1/blogs
