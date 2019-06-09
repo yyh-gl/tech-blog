6ed11e986aaccf1275e591fb998c9fc4c43c5d65
yyh-gl <yhonda.95.gl@gmail.com>
2019-06-09 23:05:31 +0900

    予約公開テスト2

--- reservation_deploy.sh
+++ reservation_deploy.sh
@@ -9,4 +9,4 @@ git add -A &&
 git commit -m "$msg" &&
 git push origin master &&
 curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"$1\"}" https://super.hobigon.work/api/v1/blogs &&
-curl -X POST -H "Content-Type: application/json" -d "{\"text\":\"$msg\"}" https://hooks.slack.com/services/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
+curl -X POST -H "Content-Type: application/json" -d "{\"text\":\"hoge\"}" https://hooks.slack.com/services/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
