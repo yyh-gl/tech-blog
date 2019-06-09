dee880141ebeca752c9f39f37f9df245c5447754
yyh-gl <yhonda.95.gl@gmail.com>
2019-06-09 22:46:59 +0900

    予約公開スクリプト修正

---
+++ reservation_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/tech-blog-settings &&
+git pull origin master&&
+hugo &&
+cd public &&
+msg="【予約公開】記事コード：$1"
+git add -A &&
+git commit -m "$msg" &&
+git push origin master &&
+curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"$1\"}" https://super.hobigon.work/api/v1/blogs &&
+curl -X POST --data-urlencode "payload={\"text\": \"$msg\" }" https://hooks.slack.com/services/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
