ab72b8adeb17dbc8a7db03311710b95df08e95a7
yyh-gl <yhonda.95.gl@gmail.com>
2019-06-10 14:04:20 +0900

    予約公開用pushスクリプト追加

---
+++ reserve.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+   then echo "引数でタイトルを指定"
+   exit -1
+fi
+
+echo "予約投稿開始 ..." &&
+
+# ブログ設定用リポジトリへのPUSH
+echo -e "\nブログ設定用リポジトリへPUSH" &&
+msg="【公開】記事コード：$1"
+git add -A &&
+git commit -m "$msg" &&
+git push origin master &&
+echo "ブログ設定用リポジトリへPUSH完了" &&
+
+echo -e "\n予約投稿完了！"
+
