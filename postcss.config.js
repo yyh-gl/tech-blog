844ffa572256298323f97da706ac1cfac970d584
yyh-gl <yhonda.95.gl@gmail.com>
2026-07-10 07:31:07 +0900

    chore(theme): remove leftover hugo-future-imperfect-slim remnants

--- postcss.config.js
+++ postcss.config.js
@@ -5,15 +5,10 @@ module.exports = {
     purgecssPlugin({
       content: [
         './layouts/**/*.html',
-        './themes/hugo-future-imperfect-slim/layouts/**/*.html',
         './content/**/*.md',
         './static/js/**/*.js',
       ],
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: {
-        // Fancybox が動的に付与するクラスを保持
-        patterns: [/^fancybox/, /^is-/, /^has-/, /^fb-/, /compensate-for-scrollbar/],
-      },
     }),
   ],
 }
